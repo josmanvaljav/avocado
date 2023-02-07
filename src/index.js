ddbd5499e140738c75cda93a94bf871c90cb4665
josmanvaljav <josmanvaljav@hotmail.com>
2023-02-07 17:03:16 -0500

    First commit avocado store with tailwind

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const mainUrl = "https://platzi-avo.vercel.app";
+const apiUrl = mainUrl + "/api/avo";
+const jsMount = document.querySelector("#js-mount");
+
+// API Intl, internationalize format Dates (DateTimeFormat) and Numbers (NumberFormat)
+const formatPrice = (price) => {
+  const newPrice = new window.Intl.NumberFormat("en-EN", {
+    style: "currency",
+    currency: "USD"
+  }).format(price);
+
+  return newPrice;
+};
+
+window
+  // Connect to a server
+  .fetch(apiUrl)
+  // process the response and parse to json
+  .then(response => response.json())
+  // JSON to Data, to render on the browser
+  .then((responseJson) => {
+    const allItems = [];
+    responseJson.data.forEach(item => {
+      // Create image node
+      const image = document.createElement("img");
+      image.src = mainUrl + item.image;
+      // Create title node
+      const title = document.createElement("h2");
+      title.textContent = item.name;
+      title.className = "text-indigo-600 text-xl font-bold";
+      // Create price node
+      const price = document.createElement("div");  
+      price.textContent = formatPrice(item.price);
+      price.className = "font-medium";
+
+      // Create a container for each group
+      const groupContainer = document.createElement("div");
+      groupContainer.className = "grid place-items-center";
+      groupContainer.append(image, title, price);
+
+      // Push each element into the array
+      allItems.push(groupContainer);
+    });
+    // print the elements at the end of the body
+    jsMount.append(...allItems);
+  });
